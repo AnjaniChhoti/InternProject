5b39cbb8570231bd409a4807808c4d9a3e29b8ca
anjani <anjanisingh321072@gmail.com>
2020-06-03 00:02:23 +0530

    first commit

---
+++ gyantu-master/career/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>GyanTU-Jobs</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel='icon' href='../images/favicon.png' type='image/x-icon'/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="../css/main.css" media="screen">
+</head>
+<script>
+    function writeUs(){
+        alert("Please share your profile on careers@gyantu.com");
+    }
+</script>
+<body>
+<div class="row"><a href="https://gyantu.com"><img src="../images/logo.png" style="margin-left:20px;width:100px; height:63px;"></a></div>
+
+<div class="container">
+  <div class="jumbotron">
+    <h1>Jobs at GyanTU</h1>      
+    <p>Join the world class team of professionals</p>
+  </div>
+  <p><h2>Come Work With Us.</h2></p>
+
+<div class="row font-19 margin-top-50">
+    <div class="col-sm-3 col-md-3 col-lg-3" style="background-color:lavender;">Engineering Manager</div>
+    <div class="col-sm-1"><button type="button" class="btn" onclick='writeUs();'>Apply</button></div>
+</div>
+<div class="row font-19 margin-top-30">
+    <div class="col-sm-3 col-md-3 col-lg-3" style="background-color:lavenderblush;">Head of Design</div>
+    <div class="col-sm-1"><button type="button" class="btn" onclick='writeUs();'>Apply</button></div>
+</div>
+<div class="row font-19 margin-top-30">
+    <div class="col-sm-3 col-md-3 col-lg-3" style="background-color:lavender;">Animator</div>
+    <div class="col-sm-1"><button type="button" class="btn" onclick='writeUs();'>Apply</button></div>
+</div>
+<div class="row margin-top-30 font-19">
+    <div class="col-sm-3 col-md-3 col-lg-3" style="background-color:lavenderblush;">Business Development Associate</div>
+    <div class="col-sm-1"><button type="button" class="btn" onclick='writeUs();'>Apply</button></div>
+</div>
+<div class="row font-19 margin-top-30">
+    <div class="col-sm-3 col-md-3 col-lg-3" style="background-color:lavender;">Data Analyst</div>
+    <div class="col-sm-1"><button type="button" class="btn" onclick='writeUs();'>Apply</button></div>
+</div>
+<div class="row font-19 margin-top-30">
+    <div class="col-sm-3 col-md-3 col-lg-3" style="background-color:lavenderblush;">Teachers(Math)</div>
+    <div class="col-sm-1"><button type="button" class="btn" onclick='writeUs();'>Apply</button></div>
+</div>
+<div class="row font-19 margin-top-30">
+    <div class="col-sm-3 col-md-3 col-lg-3" style="background-color:lavender;">Teachers(Science)</div>
+    <div class="col-sm-1"><button type="button" class="btn" onclick='writeUs();'>Apply</button></div>
+</div>
+
+
+<div class="row margin-top-30 font-19">
+    <div class="col-sm-3 col-md-3 col-lg-3" style="background-color:lavenderblush;">Software Development Engineer(Frontend)</div>
+    <div class="col-sm-1"><button type="button" class="btn" onclick='writeUs();'>Apply</button></div>
+</div>
+<div class="row font-19 margin-top-30">
+    <div class="col-sm-3 col-md-3 col-lg-3" style="background-color:lavender;">Software Development Engineer(Backend)</div>
+    <div class="col-sm-1"><button type="button" class="btn" onclick='writeUs();'>Apply</button></div>
+</div>
+<div class="row font-19 margin-top-30">
+    <div class="col-sm-3 col-md-3 col-lg-3" style="background-color:lavenderblush;">Software Tester</div>
+    <div class="col-sm-1"><button type="button" class="btn" onclick='writeUs();'>Apply</button></div>
+    
+</div>
+
+      
+</div>
+</body>
+</html>
